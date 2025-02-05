1b56dc1ee8817bc00983c16632d6de82129fcd25
PANANG6425 <100993424+PANANG6425@users.noreply.github.com>
2025-02-05 15:19:13 +0700

    Booking site
    ยังไม่เสร็จ เหลือเล็กน้อย

--- speedmaster/views.py
+++ speedmaster/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 def index(request):
     context = {'message': 'This is a dynamic message!'}
     return render(request, 'index.html',{"Message":"Hello"})
+from django.shortcuts import render
+
+def booking(request):
+    return render(request, 'booking.html')
 
