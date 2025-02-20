9b7d1d73422dbae12f34874be723bb3f5156c25d
Thanawat Ueasiripracha <thanawatueasiripracha@Thanawats-MacBook-Air.local>
2025-02-20 15:17:09 +0700

    fix register does not take email

--- user/views.py
+++ user/views.py
@@ -16,9 +16,13 @@ def dashboard(request):
         context['username'] = 'Guest'
     return render(request, 'dashboard.html', context)
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm  # Import your custom form
+
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
@@ -26,6 +30,7 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, "register.html", {"form": form})
+
 
 def login_view(request):
     if request.method == 'POST':
